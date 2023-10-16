64e9678d12c677a108a201bb443bb35270ea5b7f
Oliver <jamesonosa@cardiff.ac.uk>
2023-10-16 10:55:49 +0100

    Added items

--- gameCode/items.py
+++ gameCode/items.py
@@ -1,59 +1,103 @@
-item_id = {
-    "id": "",
+item_instruction_book = {
+    "id": "instruction book",
 
-    "name": "",
+    "name": "the instruction book",
 
-    "description":"",
+    "description":"""This book has instructions for how to assemble your monster.
+    You will need to collect the following body parts to your lab, as well as the correct materials.
+    You need to collect a right arm, a left arm, a left leg, a right leg, as well as the torso and head of your monster.
+    You need to gain a needle and thread to also properly assemble the monster. 
+    Don't forget about the foes on your way who also covett the body parts you seek, and good luck.""",
 
-    "weight": 0
+    "weight": 0.7
 }
 
-item_laptop = {
-    "id": "",
+item_needle_and_thread= {
+    "id": "needle and thread",
 
-    "name": "",
+    "name": "a needle and thread",
 
-    "description": "",
+    "description": "The needle and thread needed to assemble your monster.",
 
-    "weight": 0
+    "weight": .05
 }
 
-item_money = {
-    "id": "",
+item_rock = {
+    "id": "funny rock",
 
-    "name": "",
+    "name": "a rock",
 
-    "description": "",
+    "description": "A rock.",
 
-    "weight": 0
+    "weight": 0.3
 }
 
-item_biscuits = {
-    "id": "",
+item_baseball_bat = {
+    "id": "baseball bat",
 
-    "name": "",
+    "name": "a baseball bat",
 
-    "description": "",
+    "description": "A baseball bat to defend yourself if necessary.",
 
-    "weight": 0
+    "weight": 1
 }
 
-item_pen = {
-    "id": "",
+item_holy_water = {
+    "id": "holy water",
     
-    "name": "",
+    "name": "holy water",
 
-    "description": "",
+    "description": "Holy Water",
 
-    "weight": 0
+    "weight": 0.3
 }
 
-item_handbook = {
-    "id": "",
+item_saw = {
+    "id": "saw",
     
-    "name": "",
+    "name": "a saw",
 
-    "description": "",
+    "description": "A saw",
 
-    "weight": 0
+    "weight": 0.8
+}
+
+item_hammer = {
+    "id": "hammer",
+    
+    "name": "a hammer",
+
+    "description": "A hammer",
+
+    "weight": 0.9
+}
+
+item_spear = {
+    "id": "spear",
+    
+    "name": "a spear",
+
+    "description": "A spear",
+
+    "weight": 1
+}
+
+item_eight_pages = {
+    "id": "eight pages",
+    
+    "name": "the eight pages",
+
+    "description": "The eight pages",
+
+    "weight": 0.01
+}
+
+item_soul_jar = {
+    "id": "soul jar",
+    
+    "name": "a soul jar",
+
+    "description": "A soul jar",
+
+    "weight": 0.3
 }
