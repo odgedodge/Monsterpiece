29debfb05c4c5e8c9078694647633142382742fb
Oliver <jamesonosa@cardiff.ac.uk>
2023-10-16 15:37:17 +0100

    Added characters

---
+++ gameCode/characters.py
@@ -0,0 +1,49 @@
+character_dracula = {
+    "name": "dracula",
+    
+    "defeated_by": "holy water",
+    
+    "defending_body_part": None,
+    
+    "dialouge": []
+}
+
+character_freddy = {
+    "name": "freddy fazbear",
+    
+    "defeated_by": "baseball bat",
+    
+    "defending_body_part": None,
+    
+    "dialouge": []
+}
+
+character_grim = {
+    "name": "the grim reaper",
+    
+    "defeated_by": "making a deal",
+    
+    "defending_body_part": None,
+    
+    "dialouge": []
+}
+
+character_slenderman = {
+    "name": "slenderman",
+    
+    "defeated_by": "eight pages",
+    
+    "defending_body_part": None,
+    
+    "dialouge": []
+}
+
+character_pennywise = {
+    "name": "pennywise",
+    
+    "defeated_by": "spear",
+    
+    "defending_body_part": None,
+    
+    "dialouge": []
+}
