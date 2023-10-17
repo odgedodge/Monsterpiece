b18b9b3ae6cc42a9da812616756e233c306e91ae
Denis Bojar <bojard@cardiff.ac.uk>
2023-10-17 14:55:04 +0000

    Update dialouge.py

--- gameCode/dialouge.py
+++ gameCode/dialouge.py
@@ -1,13 +1,13 @@
-dialouge_dracula = {
+dialogue_dracula = {
     "id": "dracula",
     
     "multiple options": True,
     
-    "base dialouge": ["Hello Henry, it is good to see you again.", "I would ask how your travels went, but since you are back... well I'm sure you learnt something.", "You're here for the monster aren't you?. Again?", "Do you ever learn Henry? Don't revive dead things.", "I will not see that monster again, which is why I have hidden a leg from you. He will not be reassembled."],
+    "base dialogue": ["Hello Henry, it is good to see you again.", "I would ask how your travels went, but since you are back... well I'm sure you learnt something.", "You're here for the monster aren't you?. Again?", "Do you ever learn Henry? Don't revive dead things.", "I will not see that monster again, which is why I have hidden a leg from you. He will not be reassembled."],
     
-    "dialouge one": ["You need to be careful Henry, life and death is not to be meddled with.", "If you're sure you will be careful, do not make the same mistakes again.", "If he is rebuilt, it should be as a mememnto, a warning to not meddle outside the human domain."],
+    "dialogue one": ["You need to be careful Henry, life and death is not to be meddled with.", "If you're sure you will be careful, do not make the same mistakes again.", "If he is rebuilt, it should be as a mememnto, a warning to not meddle outside the human domain."],
     
-    "dialouge two": ["You do not want to fight me. This is a war you will not win, it is not worth it to rebuild a monster. Fighting you is a necessary evil to prevent the monster resurfacing."]
+    "dialogue two": ["You do not want to fight me. This is a war you will not win, it is not worth it to rebuild a monster. Fighting you is a necessary evil to prevent the monster resurfacing."]
 }
 
 dialouge_pennywise = {
@@ -15,24 +15,24 @@ dialouge_pennywise = {
     
     "multiple options": False,
     
-    "base dialouge": []
+    "base dialogue": []
     
 }
 
-dialouge_grim = {
+dialogue_grim = {
     "id": "the grim reaper",
     
     "multiple options": False,
     
-    "base dialouge": []
+    "base dialogue": []
 }
 
 
-dialouge_freddy = {
+dialogue_freddy = {
     "id": "freddy fazbear",
     
     "multiple options": False,
     
-    "base dialouge": []
+    "base dialogue": []
 }
 
