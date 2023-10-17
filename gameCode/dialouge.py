0ca5cd7927059e42479ca8ddb709a8ecbe76fbc5
Oliver <jamesonosa@cardiff.ac.uk>
2023-10-17 15:04:54 +0100

    Finished Dracula dialouge

--- gameCode/dialouge.py
+++ gameCode/dialouge.py
@@ -3,11 +3,11 @@ dialouge_dracula = {
     
     "multiple options": True,
     
-    "base dialouge": ["Hello Henry, it is good to see you again.", "I would ask how your travels went, but since you are back... well I'm sure you learnt something.", "You're here for the monster. Again?", "Do you ever learn Henry? Don't revive dead things.", "I will not see that monster again, which is why I have hidden a leg from you. He will not be reassembled."],
+    "base dialouge": ["Hello Henry, it is good to see you again.", "I would ask how your travels went, but since you are back... well I'm sure you learnt something.", "You're here for the monster aren't you?. Again?", "Do you ever learn Henry? Don't revive dead things.", "I will not see that monster again, which is why I have hidden a leg from you. He will not be reassembled."],
     
-    "dialouge one": [],
+    "dialouge one": ["You need to be careful Henry, life and death is not to be meddled with.", "If you're sure you will be careful, do not make the same mistakes again.", "If he is rebuilt, it should be as a mememnto, a warning to not meddle outside the human domain."],
     
-    "dialouge two": []
+    "dialouge two": ["You do not want to fight me. This is a war you will not win, it is not worth it to rebuild a monster. Fighting you is a necessary evil to prevent the monster resurfacing."]
 }
 
 dialouge_pennywise = {
