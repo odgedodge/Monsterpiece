f9dace93835c6ecd101d21f997be4c621aaec581
Denis Bojar <bojard@cardiff.ac.uk>
2023-10-17 18:54:55 +0000

    finished grim reaper dialogue

--- gameCode/dialogue.py
+++ gameCode/dialogue.py
@@ -5,7 +5,7 @@ dialogue_dracula = {
     
     "base dialogue": ["Hello Henry, it is good to see you again.", "I would ask how your travels went, but since you are back... well I'm sure you learnt something.", "You're here for the monster aren't you?. Again?", "Do you ever learn Henry? Don't revive dead things.", "I will not see that monster again, which is why I have hidden a leg from you. He will not be reassembled."],
     
-    "dialogue one": ["You need to be careful Henry, life and death is not to be meddled with.", "If you're sure you will be careful, do not make the same mistakes again.", "If he is rebuilt, it should be as a mememnto, a warning to not meddle outside the human domain."],
+    "dialogue one": ["You need to be careful Henry, life and death is not to be meddled with.", "If you're sure you will be careful, do not make the same mistakes again.", "If he is rebuilt, it should be as a memento, a warning to not meddle outside the human domain."],
     
     "dialogue two": ["You do not want to fight me. This is a war you will not win, it is not worth it to rebuild a monster. Fighting you is a necessary evil to prevent the monster resurfacing."]
 }
@@ -24,29 +24,35 @@ dialogue_grim = {
     
     "multiple options": True,
     
-    "base dialogue": ["Greetings, mortal. I am the one called Death, a harbinger of inevitable demise.","Be warned, mortal. This creature you seek to revive is a blasphemy to life itself."],
+    "base dialogue": ["Greetings, mortal. I am the one called Death, a harbinger of inevitable demise.","Be warned, mortal. This creature you seek to revive is a blasphemy to life itself.", "To obtain and possess a human torso is a grave sin, one that will surely bring down divine wrath. I, the grim reaper, must halt your misdeeds."],
 
-    "dialogue one": ["Your time has come to an end, and nothing can prevent what is written in the book of death.", "I must intervene. Do not proceed, lest you wish to face the wrath of the grim reaper."],
+    "dialogue one": ["Your time has come to an end, and nothing can prevent what is written in the book of death.", "I must intervene. Do not proceed, lest you wish to face the wrath of the grim reaper.", "Mortal, you are meddling in forces you do not understand."],
 
-    "dialogue two": ["My scythe stands tall, and I am ready to take you.", "Are you prepared for what awaits? Your life is now in my hands."]
-
-
+    "dialogue two": ["My scythe stands tall, and I am ready to take you.", "Are you prepared for what awaits? Your life is now in my hands.", "There is no mercy for you, not in this world or any other. Prepare yourself, for the reaper is coming. May the fates have mercy on your soul, for I shall have none."]
 }
 
 
 dialogue_freddy = {
     "id": "freddy fazbear",
     
-    "multiple options": False,
+    "multiple options": True,
     
-    "base dialogue": []
+    "base dialogue": [],
+
+    "dialogue one": [],
+
+    "dialogue two": []
 }
 
 
 dialogue_slenderman = {
     "id": "slenderman",
 
-    "multiple options": False,
+    "multiple options": True,
 
-    "base dialogue": []
+    "base dialogue": [],
+
+    "dialogue one": [],
+
+    "dialogue two": []
 }
