d3dfc720299120f27357f94b3ae7bc47b1c10ce4
Oliver Jameson <JamesonOSA@cardiff.ac.uk>
2023-10-18 10:25:44 +0100

    Added comments and slight changes to dracula dialouge (added player speech)

--- gameCode/dialogue.py
+++ gameCode/dialogue.py
@@ -1,11 +1,11 @@
 dialogue_dracula = {
     "id": "dracula",
     
-    "multiple options": True,
+    "multiple options": True, #Can both
     
     "base dialogue": ["Hello Henry, it is good to see you again.", "I would ask how your travels went, but since you are back... well I'm sure you learnt something.", "You're here for the monster aren't you?. Again?", "Do you ever learn Henry? Don't revive dead things.", "I will not see that monster again, which is why I have hidden a leg from you. He will not be reassembled."],
     
-    "dialogue one": ["You need to be careful Henry, life and death is not to be meddled with.", "If you're sure you will be careful, do not make the same mistakes again.", "If he is rebuilt, it should be as a memento, a warning to not meddle outside the human domain."],
+    "dialogue one": ["You need to be careful Henry, life and death is not to be meddled with.", "You: I'll be careful, he ","If you're sure you will be careful, do not make the same mistakes again.", "If he is rebuilt, it should be as a memento, a warning to not meddle outside the human domain."],
     
     "dialogue two": ["You do not want to fight me. This is a war you will not win, it is not worth it to rebuild a monster. Fighting you is a necessary evil to prevent the monster resurfacing."]
 }
@@ -13,7 +13,7 @@ dialogue_dracula = {
 dialogue_pennywise = {
     "id": "pennywise",
     
-    "multiple options": False,
+    "multiple options": False, #Can only fight
     
     "base dialogue": []
     
@@ -22,7 +22,7 @@ dialogue_pennywise = {
 dialogue_grim = {
     "id": "the grim reaper",
     
-    "multiple options": True,
+    "multiple options": False, #Can only talk
     
     "base dialogue": ["Greetings, mortal. I am the one called Death, a harbinger of inevitable demise.","Be warned, mortal. This creature you seek to revive is a blasphemy to life itself.", "To obtain and possess a human torso is a grave sin, one that will surely bring down divine wrath. I, the grim reaper, must halt your misdeeds."],
 
@@ -35,24 +35,8 @@ dialogue_grim = {
 dialogue_freddy = {
     "id": "freddy fazbear",
     
-    "multiple options": True,
+    "multiple options": False, #Can only fight
     
-    "base dialogue": ["Hello Henry"],
-
-    "dialogue one": [],
-
-    "dialogue two": []
+    "base dialogue": ["Hello Henry"]
 }
 
-
-dialogue_slenderman = {
-    "id": "slenderman",
-
-    "multiple options": True,
-
-    "base dialogue": [],
-
-    "dialogue one": [],
-
-    "dialogue two": []
-}
