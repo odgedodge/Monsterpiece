bc14e32751b2ea2fa1a11ebe0af1cce91f8b2335
Rhys <williamsr157@cardiff.ac.uk>
2023-10-23 13:56:42 +0100

    Added intro paragraph

--- gameCode/game.py
+++ gameCode/game.py
@@ -438,7 +438,10 @@ def move(exits, direction):
 def main():
     # Tell them how to skip
     print("Press S to skip.")
-
+    print("""Welcome to the haunted house, each room before you holds ancient secrets for you to unlock. 
+          Join us on an adventurous journey where you will meet suspicious individuals, some of which you might recognise from your favourite halloween productions. 
+          Along the way you will be able to talk to characters and battle some of the most famous horror villains. 
+          You are playing as Henry Frankenstein and your goal is to collect each limb of Frankenstein's monster in order to overcome the haunted house and build the monster. """)
     # Main game loop
     while True:
         # Display game status (room description, inventory etc.)
