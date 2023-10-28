fe77f193c1cf049948c35d3a15eee5cba73ba1ae
Oliver <jamesonosa@cardiff.ac.uk>
2023-10-29 00:54:00 +0100

    Made gameparser an object

--- gameparser.py
+++ gameparser.py
@@ -1,6 +1,94 @@
 import string
 
-# List of "unimportant" words (feel free to add more)
+class GameParser:
+    def __init__(self, skip_words):
+        self.__skip_words = skip_words
+    
+    #Removes unwanted words from the players input. 
+    def filter_words(self, words): 
+        """This function takes a list of words and returns a copy of the list from
+        which all words provided in the list skip_words have been removed.
+        For example:
+
+        >>> filter_words(["help", "me", "please"], ["me", "please"])
+        ['help']
+
+        >>> filter_words(["go", "south"], skip_words)
+        ['go', 'south']
+
+        >>> filter_words(['how', 'about', 'i', 'go', 'through', 'that', 'little', 'passage', 'to', 'the', 'south'], skip_words)
+        ['go', 'passage', 'south']
+
+        """
+        # Loops through for every word in the list
+        for word in list(words):
+            # Checks if word in the skip word list then removes it if true
+            if word in self.__skip_words: 
+                words.remove(word) 
+
+        # Returns the input with all skipped words removed
+        return words 
+    
+    #Removes all punctuation from player input
+    def remove_punct(self, text): 
+        """This function is used to remove all punctuation
+        marks from a string. Spaces do not count as punctuation and should
+        not be removed. The function takes a string and returns a new string
+        which does not contain any puctuation. For example:
+
+        >>> remove_punct("Hello, World!")
+        'Hello World'
+        >>> remove_punct("-- ...Hey! -- Yes?!...")
+        ' Hey  Yes'
+        >>> remove_punct(",go!So.?uTh")
+        'goSouTh'
+        """
+        #creates a new empty string
+        no_punct = "" 
+
+        #loops for every character in the input
+        for char in text: 
+            #checks if the current character is punctuation if not adds chracter to new string
+            if not (char in string.punctuation): 
+                no_punct = no_punct + char
+
+        #returns the new string that contains no punctuation
+        return no_punct
+    
+    #Normalises the users inpput to reduce it to a lsit of desired words
+    def normalise_input(self, user_input): 
+        """This function removes all punctuation from the string and converts it to
+        lower case. It then splits the string into a list of words (also removing
+        any extra spaces between words) and further removes all "unimportant"
+        words from the list of words using the filter_words() function. The
+        resulting list of "important" words is returned. For example:
+
+        >>> normalise_input("  Go   south! ")
+        ['go', 'south']
+        >>> normalise_input("!!!  tAkE,.    LAmp!?! ")
+        ['take', 'lamp']
+        >>> normalise_input("HELP!!!!!!!")
+        ['help']
+        >>> normalise_input("Now, drop the sword please.")
+        ['drop', 'sword']
+        >>> normalise_input("Kill ~ tHe :-  gObLiN,. wiTH my SWORD!!!")
+        ['kill', 'goblin', 'sword']
+        >>> normalise_input("I would like to drop my laptop here.")
+        ['drop', 'laptop']
+        >>> normalise_input("I wish to take this large gem now!")
+        ['take', 'gem']
+        >>> normalise_input("How about I go through that little passage to the south...")
+        ['go', 'passage', 'south']
+
+        """
+        #Remove punctuation and convert to lower case
+        no_punct = self.remove_punct(user_input).lower() 
+        #Splits string into a list of words and removes unwanted words
+        normalised = self.filter_words(no_punct.split(), self.__skip_words) 
+        #returns the normalised input
+        return normalised 
+    
+# List of "unimportant" words
 skip_words = ['a', 'about', 'all', 'an', 'another', 'any', 'around', 'at',
               'bad', 'beautiful', 'been', 'better', 'big', 'can', 'every', 'for',
               'from', 'good', 'have', 'her', 'here', 'hers', 'his', 'how',
@@ -11,88 +99,7 @@ skip_words = ['a', 'about', 'all', 'an', 'another', 'any', 'around', 'at',
               'towards', 'until', 'us', 'want', 'we', 'what', 'when', 'why',
               'wish', 'with', 'would']
 
-#Removes unwanted words from the players input. 
-def filter_words(words, skip_words): 
-    """This function takes a list of words and returns a copy of the list from
-    which all words provided in the list skip_words have been removed.
-    For example:
-
-    >>> filter_words(["help", "me", "please"], ["me", "please"])
-    ['help']
-
-    >>> filter_words(["go", "south"], skip_words)
-    ['go', 'south']
-
-    >>> filter_words(['how', 'about', 'i', 'go', 'through', 'that', 'little', 'passage', 'to', 'the', 'south'], skip_words)
-    ['go', 'passage', 'south']
-
-    """
-    #Loops through for every word in the list
-    for word in list(words):
-         #checks if word in the skip word list then removes it if true
-        if word in skip_words: 
-            words.remove(word) 
-
-    #returns the input with all skipped words removed
-    return words 
+game_parser = GameParser(skip_words)
 
 
-#Removes all punctuation from player input
-def remove_punct(text): 
-    """This function is used to remove all punctuation
-    marks from a string. Spaces do not count as punctuation and should
-    not be removed. The function takes a string and returns a new string
-    which does not contain any puctuation. For example:
 
-    >>> remove_punct("Hello, World!")
-    'Hello World'
-    >>> remove_punct("-- ...Hey! -- Yes?!...")
-    ' Hey  Yes'
-    >>> remove_punct(",go!So.?uTh")
-    'goSouTh'
-    """
-    #creates a new empty string
-    no_punct = "" 
-
-    #loops for every character in the input
-    for char in text: 
-        #checks if the current character is punctuation if not adds chracter to new string
-        if not (char in string.punctuation): 
-            no_punct = no_punct + char
-
-    #returns the new string that contains no punctuation
-    return no_punct 
-
-
-#Normalises the users inpput to reduce it to a lsit of desired words
-def normalise_input(user_input): 
-    """This function removes all punctuation from the string and converts it to
-    lower case. It then splits the string into a list of words (also removing
-    any extra spaces between words) and further removes all "unimportant"
-    words from the list of words using the filter_words() function. The
-    resulting list of "important" words is returned. For example:
-
-    >>> normalise_input("  Go   south! ")
-    ['go', 'south']
-    >>> normalise_input("!!!  tAkE,.    LAmp!?! ")
-    ['take', 'lamp']
-    >>> normalise_input("HELP!!!!!!!")
-    ['help']
-    >>> normalise_input("Now, drop the sword please.")
-    ['drop', 'sword']
-    >>> normalise_input("Kill ~ tHe :-  gObLiN,. wiTH my SWORD!!!")
-    ['kill', 'goblin', 'sword']
-    >>> normalise_input("I would like to drop my laptop here.")
-    ['drop', 'laptop']
-    >>> normalise_input("I wish to take this large gem now!")
-    ['take', 'gem']
-    >>> normalise_input("How about I go through that little passage to the south...")
-    ['go', 'passage', 'south']
-
-    """
-    #Remove punctuation and convert to lower case
-    no_punct = remove_punct(user_input).lower() 
-    #Splits string into a list of words and removes unwanted words
-    normalised = filter_words(no_punct.split(), skip_words) 
-    #returns the normalised input
-    return normalised 
