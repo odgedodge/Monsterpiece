f8c155421277947f68101d9cde3aadb01483bd4e
Oliver <jamesonosa@cardiff.ac.uk>
2023-10-29 01:32:08 +0000

    made skip words hard-coded (no globals)

--- gameparser.py
+++ gameparser.py
@@ -1,8 +1,17 @@
 import string
 
 class GameParser:
-    def __init__(self, skip_words):
-        self.__skip_words = skip_words
+    def __init__(self):
+        # List of "unimportant" words
+        self.__skip_words = ['a', 'about', 'all', 'an', 'another', 'any', 'around', 'at',
+              'bad', 'beautiful', 'been', 'better', 'big', 'can', 'every', 'for',
+              'from', 'good', 'have', 'her', 'here', 'hers', 'his', 'how',
+              'i', 'if', 'in', 'into', 'is', 'it', 'its', 'large', 'later',
+              'like', 'little', 'main', 'me', 'mine', 'more', 'my', 'now',
+              'of', 'off', 'oh', 'on', 'please', 'small', 'some', 'soon',
+              'that', 'the', 'then', 'this', 'those', 'through', 'till', 'to',
+              'towards', 'until', 'us', 'want', 'we', 'what', 'when', 'why',
+              'wish', 'with', 'would']
     
     #Removes unwanted words from the players input. 
     def filter_words(self, words): 
@@ -87,19 +96,8 @@ class GameParser:
         normalised = self.filter_words(no_punct.split(), self.__skip_words) 
         #returns the normalised input
         return normalised 
-    
-# List of "unimportant" words
-skip_words = ['a', 'about', 'all', 'an', 'another', 'any', 'around', 'at',
-              'bad', 'beautiful', 'been', 'better', 'big', 'can', 'every', 'for',
-              'from', 'good', 'have', 'her', 'here', 'hers', 'his', 'how',
-              'i', 'if', 'in', 'into', 'is', 'it', 'its', 'large', 'later',
-              'like', 'little', 'main', 'me', 'mine', 'more', 'my', 'now',
-              'of', 'off', 'oh', 'on', 'please', 'small', 'some', 'soon',
-              'that', 'the', 'then', 'this', 'those', 'through', 'till', 'to',
-              'towards', 'until', 'us', 'want', 'we', 'what', 'when', 'why',
-              'wish', 'with', 'would']
 
-game_parser = GameParser(skip_words)
+
 
 
 
